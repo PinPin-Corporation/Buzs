7b2e5bf84f16fb0d7340207b4a45792167dbd9e4
Nicolas Saysset <nicolassaysset@free.fr>
2020-04-11 02:50:56 +0200

    m

--- panier.php
+++ panier.php
@@ -12,19 +12,7 @@
         <title>Busz - panier</title>
     </head>
     <body>
-    <header>
-        <div class="wrapper">
-            <h1>Busz <span id="red">•</span></h1>
-            <nav>
-                <ul>
-                    <li><a href="index.html">Accueil<span id="red">   |</span></a></li>
-                    <li><a href="search.php">Catégories<span id="red">   |</span></a></li>
-                    <li><a href="#">Contact</a><span id="red">   |</span></li>
-                    <li><a href="panier.php">Panier </a><span id="red">   </span></li>
-                </ul>
-            </nav>
-            </div>
-        </header>
+        <?php include('menu.php');?>
         <div>
             <h2>Panier</h2>
         </div>
@@ -39,10 +27,6 @@
                 Bouton payer avec php
             -->
         </div>
-        <footer>
-            <div class="wrapper">
-                <h1>Busz</h1>
-            </div>
-        </footer>
+        <?php include('footer.php');?>
     </body>
 </html>
