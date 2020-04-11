2acf7fd9d65171e85f2a488eef95b4dbce3cda49
Nicolas Saysset <nicolassaysset@free.fr>
2020-04-11 02:52:41 +0200

    m

--- profile.php
+++ profile.php
@@ -8,19 +8,7 @@
         <title>Busz - profile</title>
     </head>
     <body>
-    <header>
-        <div class="wrapper">
-            <h1>Busz <span id="red">•</span></h1>
-            <nav>
-                <ul>
-                    <li><a href="index.html">Accueil<span id="red">   |</span></a></li>
-                    <li><a href="search.php">Catégories<span id="red">   |</span></a></li>
-                    <li><a href="#">Contact</a><span id="red">   |</span></li>
-                    <li><a href="panier.php">Panier </a><span id="red">   </span></li>
-                </ul>
-            </nav>
-            </div>
-        </header>
+        <?php include('menu.php');?>
         <form>
             <input type="text" placeholder="Nom">
             <input type="text" placeholder="Prenom">
@@ -28,10 +16,6 @@
             <input type="text" placeholder="Mot de passe">
             <input type="button" value="Changer votre mot de passe">
         </form>
-        <footer>
-            <div class="wrapper">
-            <h1>Busz</h1>
-            </div>
-        </footer>
+        <?php include('footer.php'); ?>
     </body>
 </html>
