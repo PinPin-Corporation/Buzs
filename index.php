d9a1ef5892aa1976896c0dd417e6e2f36e283816
Nicolas Saysset <nicolassaysset@free.fr>
2020-04-13 00:51:47 +0200

    m

--- index.php
+++ index.php
@@ -1,6 +1,30 @@
 <?php
-// Sous WAMP (Windows)
-$bdd = new PDO('mysql:host=localhost:8080;dbname=QueenSeduction;charset=utf8', 'root', '');
 
+try
+{
+    $bdd = new PDO('mysql:host=localhost;dbname=QueenSeduction;charset=utf8', 'root', '');
+    $reponse = $bdd->query('SELECT * FROM Articles ORDER BY Date');
+
+    while ($donnees = $reponse->fetch())
+    {
+    ?>
+    <div style="width: 1000px; height: 500px;">
+        <div style="float: left; position: relative;">
+            <img src=<?php echo '"images/' + $donnees['image'] +'"'?>>
+        </div>
+        <div style="float: right; position: relative;">
+            <h1><?php echo $donnees['nom']; ?></h1>
+            <p><?php echo $donnees['description'];?></p>
+        </div>
+    </div>
+    <php
+    }
+
+    $reponse->closeCursor(); // Termine le traitement de la requête
+    }
+catch (Exception $e)
+{
+        die('Erreur : ' . $e->getMessage());
+} 
 
 ?>
