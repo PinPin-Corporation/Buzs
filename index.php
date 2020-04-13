7172bd98108c0773954377c30dca2e303f944f72
Nicolas Saysset <nicolassaysset@free.fr>
2020-04-13 05:25:40 +0200

    commit

--- index.php
+++ index.php
@@ -44,8 +44,8 @@
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav" id="items">
           <a class="nav-item nav-link active" href="#">Accueil <span class="sr-only">(current)</span></a>
-          <a class="nav-item nav-link" href="#">Article</a>
-          <a class="nav-item nav-link" href="#">Formation</a>
+          <a class="nav-item nav-link" href="articles.php">Articles</a>
+          <a class="nav-item nav-link" href="#">Formations</a>
           <a class="nav-item nav-link" href="#">Contacts</a>
         </div>
       </div>
@@ -57,7 +57,6 @@
 
     </div>
 
-    <section id="main">
       <script type="text/javascript">
         $(window).on('load',function(){
             $('#exampleModal').modal('show');
@@ -130,30 +129,99 @@
                 die('Erreur : ' . $e->getMessage());
         } 
     ?>
-    </section>
     <div id="color">
-    <footer>
-      <div id="about" class="w-25-d-block w-50-md-inline-block">
-        <h1>Qui suis-je?</h1>
-        <h1>Queen Seduction.</h1>
-        <p>" Pour s'éprendre d'une femme, il faut qu'il y ait en elle un désert, une absence, quelque chose qui appelle la tourmente, la jouissance. Une zone de vie non entamée dans sa vie, une terre non brûlée, ignorée d'elle-même comme de vous. "</p>
-        <div id="reseausocfooter">
-          <div id="instafooter">
-    
+    <footer class="general-footer">
+      <div class="footer-mask"></div>
+      <div class="footer-inner">
+          <div class="container">
+              <div class="row">
+                  <div class="col-md-4 col-lg-4 col-sm-12 col-xs-12 footer-block">
+                      <div class="">
+                          <div class="footer-widget-content about-widget">
+                              <div class="widget-title">
+                                  <h2>Qui suis-je?</h2>
+                              </div>
+                              <div class="widget-about-site-logo">
+                                  <span id="red">Queen-Séduction</span>
+                              </div>
+                              <div class="widget-content">
+                                  <p>Pour s'éprendre d'une femme, il faut qu'il y ait en elle un désert, une absence, quelque chose qui appelle la tourmente, la jouissance. Une zone de vie non entamée dans sa vie, une terre non brûlée, ignorée d'elle-même comme de vous. </p>
+                              </div>
+                              <div class="social-networks">
+                                  <ul class="social-links">
+                                    
+                                  </ul>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="col-md-4 col-lg-4 col-sm-12 col-xs-12 footer-block">
+                      <div class="">
+                          <div class="footer-widget-content">
+                              <div class="widget-title">
+                                  <h2>Navigation</h2>
+                              </div>
+                              <div class="widget-content">
+                                  <ul class="widget-category-listings">
+                                      <li><a href="#">Accueil</a></li>
+                                      <li><a href="articles.php">Articles</a></li>
+                                      <li><a href="#">Formations</a></li>
+                                      <li><a href="#">Contact</a></li>
+                                  </ul>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="col-md-4 col-lg-4 col-sm-12 col-xs-12 footer-block">
+                      <div class="">
+                          <div class="footer-widget-content">
+                              <div class="widget-title">
+                                  <h2>Article populaire</h2>
+                              </div>
+                              <div class="widget-content">
+                                  <div class="widget-posts">
+                                      <div class="post-title">
+                                          <h5><a href="#">Contrôler l'Éjaculation et Durer Longtemps au Lit</a></h5>
+                                      </div>
+                                      <div class="posted-date">
+                                          <span><a href="#">13 Avril 2020</a></span>
+                                      </div>
+                                  </div>
+                                  <div class="widget-posts">
+                                      <div class="post-title">
+                                          <h5><a href="#">Le guide pour devenir le meilleur coup du siècle</a></h5>
+                                      </div>
+                                      <div class="posted-date">
+                                          <span><a href="#">13 Avril 2020</a></span>
+                                      </div>
+                                  </div>
+                                  <div class="widget-posts">
+                                      <div class="post-title">
+                                          <h5><a href="#">Comment séduire par message?</a></h5>
+                                      </div>
+                                      <div class="posted-date">
+                                          <span><a href="#">13 Avril 2020</a></span>
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="copyright-and-nav-row">
+                  <div class="row">
+                      <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                          <div class="copyrights">
+                             <a href="cgu.php">CONDITIONS GÉNÉRALES DE VENTE ET D’UTILISATION</a>
+                              <p>Copyrights @ 2020 Queen Seduction</p>
+                          </div>
+                      </div>
+                  </div>
+              </div>
           </div>
-        </div>
       </div>
-      <div id="navigation" class="w-25-d-block w-50-md-inline-block">
-      <h1>Nav</h1>
-
-
-      </div>
-      <div id="article" class="w-25-d-block w-50-md-inline-block">
-          <h2>Les articles populaires</h2>
-      </div>
-
-        <h2> Copyrights @ 2020 </h2>
-        <a href=""> Les conditions d'utilisations</a>
-    </footer>
+  </footer>
     </div>
   </body>
+
+</html>
---
+++ menu.php
@@ -0,0 +1,57 @@
+<head>
+    <title>Queen Seduction</title>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">  
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
+
+
+    <link href="https://fonts.googleapis.com/css2?family=Old+Standard+TT&display=swap" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="styles/style.css">
+    <script src="js/script.js"></script>
+    <link rel="shortcut icon" href="images/icone.png" />
+  </head>
+
+
+
+
+  <body>
+
+    <header> 
+
+      <div id="reseau">
+        <div id="insta">
+          <a href="https://www.instagram.com/queen_seduction_/"><img src="images/logoinsta.png"></a>
+        </div>
+      </div>
+
+      <div id="logo">
+        <img src="images/logo.png" class="img-fluid">
+      </div>
+
+    </header>
+
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav" id="items">
+          <a class="nav-item nav-link active" href="#">Accueil <span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link" href="#">Article</a>
+          <a class="nav-item nav-link" href="#">Formation</a>
+          <a class="nav-item nav-link" href="#">Contacts</a>
+        </div>
+      </div>
+    </nav>
+
+    <div id="bg">
+
+        <img src="images/caro1.jpg" id="caro" class="img-fluid">
+
+    </div>
