abe977bb0480d42f6bec28b4c86ed915cd2b92c1
Nicolas Saysset <nicolassaysset@free.fr>
2020-04-13 02:22:15 +0200

    m

--- index.php
+++ index.php
@@ -1,30 +1,78 @@
-<?php
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="styles/style.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" crossorigin="anonymous">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" crossorigin="anonymous"></script>
+        <script src="javascripts/jquery-3.5.0.js"></script>
+    </head>
+    <body>
+        <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Launch demo modal
+</button>
 
-try
-{
-    $bdd = new PDO('mysql:host=localhost;dbname=QueenSeduction;charset=utf8', 'root', '');
-    $reponse = $bdd->query('SELECT * FROM Articles ORDER BY Date');
-
-    while ($donnees = $reponse->fetch())
-    {
-    ?>
-    <div style="width: 1000px; height: 500px;">
-        <div style="float: left; position: relative;">
-            <img src=<?php echo '"images/' + $donnees['image'] +'"'?>>
-        </div>
-        <div style="float: right; position: relative;">
-            <h1><?php echo $donnees['nom']; ?></h1>
-            <p><?php echo $donnees['description'];?></p>
-        </div>
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
     </div>
-    <?php
-    }
+  </div>
+</div>
+        <?php
+            try
+            {
+                $bdd = new PDO('mysql:host=localhost;dbname=QueenSeduction;charset=utf8', 'root', '');
+                $reponse = $bdd->query('SELECT * FROM Articles ORDER BY Date DESC');
+                $donnees = $reponse->fetch();
+                ?>
 
-    $reponse->closeCursor(); // Termine le traitement de la requête
-    }
-catch (Exception $e)
-{
-        die('Erreur : ' . $e->getMessage());
-} 
+                <div id="Major_Content">
+                        <div id="Major_Content_Left">
+                            <a href=<?php echo '"' . $donnees['id'] . '.php"'?>><img src=<?php echo '"images/' . $donnees['image'] .'"'?>></a>
+                        </div>
+                        <div id="Major_Content_Right">
+                            <a href=<?php echo '"' . $donnees['id'] . '.php"'?>><h1><?php echo $donnees['nom']; ?></h1></a>
+                            <p><?php echo $donnees['description'];?></p>
+                        </div>
+                </div>
+                <?php
 
-?>
+                while ($donnees = $reponse->fetch())
+                {
+                ?>
+                <div class="Minor_Content">
+                        <div class="Minor_Content_Left">
+                            <a href=<?php echo '"' . $donnees['id'] . '.php"'?>><img src=<?php echo '"images/' . $donnees['image'] .'"'?>></a>
+                        </div>
+                        <div class="Minor_Content_Right">
+                            <a href=<?php echo '"' . $donnees['id'] . '.php"'?>><h1><?php echo $donnees['nom']; ?></h1></a>
+                            <p><?php echo $donnees['description'];?></p>
+                        </div>
+                </div>
+                <?php
+                }
+
+                $reponse->closeCursor(); // Termine le traitement de la requête
+                }
+            catch (Exception $e)
+            {
+                    die('Erreur : ' . $e->getMessage());
+            } 
+        ?>
+    </body>
+</html>
