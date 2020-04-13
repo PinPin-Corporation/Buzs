019bc1dcf92128024798e6c830504c6f26d422d5
Nicolas Saysset <nicolassaysset@free.fr>
2020-04-13 17:58:37 +0200

    m

--- menu.php
+++ menu.php
@@ -15,6 +15,22 @@
     <link rel="stylesheet" href="styles/style.css">
     <script src="js/script.js"></script>
     <link rel="shortcut icon" href="images/icone.png" />
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-163411784-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-163411784-1');
+    </script>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script>
+      (adsbygoogle = window.adsbygoogle || []).push({
+        google_ad_client: "ca-pub-8679914437126860",
+        enable_page_level_ads: true
+      });
+    </script>
   </head>
 
 
