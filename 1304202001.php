08fb4e425c91eac9ef211bf2006936833d3b5bb1
Nicolas Saysset <nicolassaysset@free.fr>
2020-04-18 13:10:20 +0200

    m

--- 1304202001.php
+++ 1304202001.php
@@ -9,10 +9,9 @@
     </form>
   </div>
 </div>
-
 <div class="container" style="max-width: 1000px;">
-    <div class="introduction">
-        <h1>Contrôler l'éjaculation et durer longtemps au lit</h1>
+  <h1 class="sticky">Contrôler l'éjaculation et durer longtemps au lit</h1>
+    <div id="introduction">
         <img src="images/femme_fatale.jpg" class="w-100">
         <p>Dans cet article, je vous dévoile les 7 erreurs les plus fréquentes que commettent les hommes en drague de rue et vous explique comment les corriger quand vous abordez une fille.<br>
 
@@ -20,20 +19,22 @@
             
             Les 7 erreurs à éviter quand vous abordez une fille dans la rue, c’est parti !</p>
     </div>
-    <h2 class="Titre_section">La fille GO FAST</h2>
-    <p class="paragraphe">Cette fille a peur. Pas forcément de vous, mais par réflexe, par habitude, par ambiance de merde généralisée, elle a appris à regarder ses pieds et à battre Usaïn Bolt sur 100m dans la rue.<br>
-
-        La première erreur à éviter, c’est d’essayer d’aborder une fille qui sprinte, qui est au téléphone, ou qui a ses écouteurs et qui garde son regard vissé sur les pavés… sans croiser son regard.<br>
-        
-        Ne vous mettez pas sur son chemin, ce serait pire qu’une manchette de l’Undertaker ou de John Cena.<br>
-        
-        Ne la suivez pas, vous n’êtes pas un stalker : si vous ne croisez pas son regard, ne faites pas comme les mecs de la Croix Rouge ou d’Action contre la Faim qui vous sautent dessus pour vous expliquer comment faire des dons.<br>
-        
-        Le meilleur moyen pour éviter de prendre un gros vent sur son passage est de repérer cette fille-là à 10 mètres, et de tenter un eye contact et un coucou !<br>
-        
-        Lui parler alors qu’elle a des écouteurs sur les oreilles revient à vouloir raisonner un alcoolique après 10 verres : c’est une mauvaise idée !<br>
-        
-        Le mieux à faire si vous débutez, quand vous voulez draguer dans la rue ou au parc, c’est d’aborder une fille immobile !
-    </p>
+    <section>
+      <h1 class="Titre_section">La fille GO FAST</h1>
+      <p class="paragraphe">Cette fille a peur. Pas forcément de vous, mais par réflexe, par habitude, par ambiance de merde généralisée, elle a appris à regarder ses pieds et à battre Usaïn Bolt sur 100m dans la rue.<br>
+  
+          La première erreur à éviter, c’est d’essayer d’aborder une fille qui sprinte, qui est au téléphone, ou qui a ses écouteurs et qui garde son regard vissé sur les pavés… sans croiser son regard.<br>
+          
+          Ne vous mettez pas sur son chemin, ce serait pire qu’une manchette de l’Undertaker ou de John Cena.<br>
+          
+          Ne la suivez pas, vous n’êtes pas un stalker : si vous ne croisez pas son regard, ne faites pas comme les mecs de la Croix Rouge ou d’Action contre la Faim qui vous sautent dessus pour vous expliquer comment faire des dons.<br>
+          
+          Le meilleur moyen pour éviter de prendre un gros vent sur son passage est de repérer cette fille-là à 10 mètres, et de tenter un eye contact et un coucou !<br>
+          
+          Lui parler alors qu’elle a des écouteurs sur les oreilles revient à vouloir raisonner un alcoolique après 10 verres : c’est une mauvaise idée !<br>
+          
+          Le mieux à faire si vous débutez, quand vous voulez draguer dans la rue ou au parc, c’est d’aborder une fille immobile !
+      </p>
+    </section>
 </div>
 <?php include('footer.php'); ?>
--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     </div>
       <script type="text/javascript">
         $(window).on('load',function(){
-            //$('#exampleModal').modal('show');
+            $('#exampleModal').modal('show');
         });
     </script>
 <!-- Modal -->
@@ -16,7 +16,7 @@
 <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
 <div class="modal-content">
   <div class="modal-header">
-    <h2 class="modal-title" id="exampleModalLabel">Entrer Entrez votre email pour recevoir gratuitement "Construire une carrière de Freelance stable et rentable"</h2>
+    <h2 class="modal-title" id="exampleModalLabel">Entrer votre email pour ne pas manquer la publication de notre nouveau post !</h2>
     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
       <span aria-hidden="true">&times;</span>
     </button>
@@ -24,11 +24,11 @@
   <div class="modal-body">
     <form action="merci.php" method="get">
         <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="mail">
-            <small id="emailHelp" class="form-text text-muted">Vous recevrez un email par jour pendant 7 jours - puis vous recevrez ma newsletter hebdomadaire. Vos informations ne seront jamais cédées à des tiers.</small>
+            <label for="exampleInputEmail1">Adresse mail</label>
+            <input type="email" required class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Entrer votre email" name="mail">
+            <small id="emailHelp" class="form-text text-muted">Vous recevrez un email dès qu'un nouveau post sortira ! Vos informations ne seront jamais cédées à des tiers.</small>
         </div>
-        <style data-emotion-css="1yauslj">.css-1yauslj{-webkit-align-items:center;-webkit-box-align:center;-ms-flex-align:center;align-items:center;display:-webkit-inline-box;display:-webkit-inline-flex;display:-ms-inline-flexbox;display:inline-flex;-webkit-flex-direction:column;-ms-flex-direction:column;flex-direction:column;-webkit-box-pack:center;-webkit-justify-content:center;-ms-flex-pack:center;justify-content:center;border-style:solid;border-width:1px;text-align:center;font-family:'Lato',sans-serif;text-shadow:none;-webkit-transition:color 0.2s linear,background 0.2s linear,border-color 0.2s linear, text-shadow 0.2s linear;transition:color 0.2s linear,background 0.2s linear,border-color 0.2s linear, text-shadow 0.2s linear;-webkit-text-decoration:none;text-decoration:none;width:100%;font-weight:bold;min-height:48px;min-width:96px;padding:14px 24px;font-family:Roboto;}.css-1yauslj,.css-1yauslj:focus,.css-1yauslj:active{border-color:rgb(254,210,0);background:rgb(254,210,0);color:rgb(0,0,0);}.css-1yauslj:hover{background-color:rgb(203,168,0);border-color:rgb(203,168,0);color:rgb(0,0,0);}</style>
+        <style data-emotion-css="1yauslj">.css-1yauslj{-webkit-align-items:center;-webkit-box-align:center;-ms-flex-align:center;align-items:center;display:-webkit-inline-box;display:-webkit-inline-flex;display:-ms-inline-flexbox;display:inline-flex;-webkit-flex-direction:column;-ms-flex-direction:column;flex-direction:column;-webkit-box-pack:center;-webkit-justify-content:center;-ms-flex-pack:center;justify-content:center;border-style:solid;border-width:1px;text-align:center;font-family:'Lato',sans-serif;text-shadow:none;-webkit-transition:color 0.2s linear,background 0.2s linear,border-color 0.2s linear, text-shadow 0.2s linear;transition:color 0.2s linear,background 0.2s linear,border-color 0.2s linear, text-shadow 0.2s linear;-webkit-text-decoration:none;text-decoration:none;width:100%;font-weight:bold;min-height:48px;min-width:96px;padding:14px 24px;font-family:Roboto;}.css-1yauslj,.css-1yauslj:focus,.css-1yauslj:active{border-color:rgb(254,210,0);background:rgb(0,210,0);color:rgb(0,0,0);}.css-1yauslj:hover{background-color:rgb(0,230,0);border-color:rgb(203,168,0);color:rgb(0,0,0);}</style>
         <button type="submit" class="css-1yauslj">Recevoir</button>
     </form>
   </div>
@@ -62,10 +62,10 @@
                         {
                         ?>
                         <div style="border-top: 1px solid grey; ">
-                            <div class="Minor_Content_Left">
+                            <div class="w-50 float-left">
                                 <a href=<?php echo '"' . $donnees['id'] . '.php"'?>><img class="img-fluid" src=<?php echo '"images/' . $donnees['image'] .'"'?>></a>
                             </div>
-                            <div class="Minor_Content_Right">
+                            <div class="W-50 float-right">
                                 <a href=<?php echo '"' . $donnees['id'] . '.php"'?>><h1><?php echo $donnees['nom']; ?></h1></a>
                                 <p><?php echo $donnees['description'];?></p>
                             </div>
