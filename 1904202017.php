65a48061b776a9eece3ac8bc012b00bc0d6c1f81
Nicolas Saysset <nicolassaysset@free.fr>
2020-04-19 20:49:05 +0200

    m

--- 1904202017.php
+++ 1904202017.php
@@ -5,24 +5,60 @@
     <div id="introduction">
         <img src="images/caro3.jpg" class="w-100">
         <p>
-
+        Vous faîtes partie des hommes qui ont toujours eu 
+        du mal à déchiffrer les signes d’une fille s’intéresse
+        à eux ? Vous avez une fille en vue avec qui vous parlez
+        mais vous ne savez pas si vous pouvez aller plus loin ?
+        Alors cette article est fait pour vous. Lisez cette article 
+        et apprenez à repérez ces signes pour enfin les utiliser à votre avantage.
         </p>
     </div>
     <section>
       <h1 class="Titre_section">Son regard</h1>
       <p class="paragraphe">
+      <b>Vous regarder intensément dans les yeux ou détourner le regard.</b> Ce type de signal est à 
+      prendre avec précaution. En effet, tout dépend de la personnalité de la fille et de la présence 
+      ou non d’autres signes d’intérêt. Par exemple, si la fille est timide elle aura tendance à détourner 
+      le regard. Cependant, si elle détourne le regard et que vous voyez que son visage commence à rougir, 
+      alors il y a de fortes chances qu’elle ressente quelque chose pour vous. <br><br>
+      <b>le clignement rapide de ses yeux.</b> Lorsque vous tapez dans l’oeil d’une fille, son cerveau libère de la dopamine 
+      c’est cette dernière qui provoque cette excitation émotionnelle de l’œil. <br><br>
+      Lorsque son attirance pour vous est très prononcée les yeux d’une fille peuvent s’écarquiller lorsqu’elle ressent une 
+      forte attraction physique. Ils montrent l’impatience de vouloir plus. <br><br>
+      <b>Les pupilles </b> : des études ont montré que les pupilles des femmes se dilatent à la vue d’une photo d’un bel homme. 
+      Si vous voyez les pupilles d’une fille se dilater, c’est qu’elle attirer par vous . Attention à la lumière ambiante, 
+      les pupilles se dilatent ou se contractent en fonction de celle-ci aussi.
       </p>
     </section>
 
     <section>
         <h1 class="Titre_section">Ses lèvres</h1>
         <p class="paragraphe">
+          Passons aux lèvres. Une femme intéressée aura tendance à : <br><br>
+          <b>S’humecter les lèvres plus souvent</b> (pas nécessairement un signe d’intérêt. 
+          Par exemple, la fille peut simplement avoir les lèvres sèches) <br><br>
+          <b>Vous sourire avec sincérité </b> (sourire de Duchenne) <br><br>
+          <b>Se mordre les lèvres.</b> Là clairement, elle a vraiment envie de vous et c’est le 
+          signe que vous pouvez l’embrasser.
         </p>
       </section>
 
       <section>
         <h1 class="Titre_section">Son corps</h1>
         <p class="paragraphe">
+        Une fille intéressé va inconsciemment : <br><br>
+        Se pencher vers vous, tourner son corps vers vous, réduire la distance qui vous sépare, 
+        en gros elle va tout faire pour être plus en contact avec vous. <br><br>
+        <b>Imiter plusieurs de vos mouvements </b>, si elle est
+        clairement sur la même longueur d’onde que vous elle va inconsciemment copier votre position 
+        ou plusieurs de vos mouvements. Si vous observez un tel comportement de synchronisation
+        chez une fille, testez-la en changeant de position. Si elle vous suit quelques secondes après, 
+        c’est un signe d'interet. <br><br>
+        <b>Rechercher le contact physique </b> en vous touchant plus régulièrement, voir même en trouvant des excuses 
+        pour vous toucher <br><br>
+        Si vous la croisez du regard et qu’<b>elle se recoiffe </b>, se caresse les cheveux, il 
+        s’agit-là dans un contexte de séduction (et seulement dans un contexte de seduction),
+        d’un marqueur extrêmement positif.
         </p>
       </section>
 
